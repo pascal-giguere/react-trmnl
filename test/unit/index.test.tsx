7c5803176c8805c6943a1b35bd563e6731e1584b
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2025-02-23 11:51:44 -0500

    Add rendering tests

--- test/unit/index.test.tsx
+++ test/unit/index.test.tsx
@@ -1,9 +1,19 @@
 import React, { type ReactElement, Fragment } from "react";
-import { render } from "../../src/index.mjs";
+import { ImageFormat, render } from "../../src/index.mjs";
 
 const TestComponent = (): ReactElement => <Fragment>Hello, world!</Fragment>;
 
-it("basic test", async () => {
-  const image: Buffer = await render(<TestComponent />);
-  expect(image).toHaveLength(0);
+it("renders a raw image with the appropriate size", async () => {
+  const image: Buffer = await render(<TestComponent />, { width: 800, height: 480, format: ImageFormat.Raw });
+  expect(image).toHaveLength(800 * 480);
+});
+
+it("renders an uncompressed bitmap image with the appropriate size", async () => {
+  const image: Buffer = await render(<TestComponent />, { width: 800, height: 480, format: ImageFormat.BMP });
+  expect(image).toHaveLength(48064);
+});
+
+it("renders a compressed PNG image with the appropriate size", async () => {
+  const image: Buffer = await render(<TestComponent />, { width: 800, height: 480, format: ImageFormat.PNG });
+  expect(image).toHaveLength(7101);
 });
