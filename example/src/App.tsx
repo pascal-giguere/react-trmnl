e0db3713012cb00104930714cfb5b79eb5fef79a
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2025-03-04 15:52:16 -0500

    Update example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,15 @@
 import React, { type ReactElement } from "react";
-import { render } from "react-trmnl";
+import { render, Box, Text, Image, Color } from "react-trmnl";
 
 const App = (): ReactElement => {
-  return <text>Hello, world!</text>;
+  return (
+    <Box left={20} top={50} width={400} height={200} backgroundColor={Color.White} borderColor={Color.Black}>
+      <Text width={400} height={200}>
+        Hello, world!
+      </Text>
+      <Image src="foo.png" width={400} height={200} />
+    </Box>
+  );
 };
 
 export const renderApp = () => render(<App />);
