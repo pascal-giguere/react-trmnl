6f6198dd5ebff259ab7dcf4a6bf029b84e483f5a
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2025-05-09 17:10:46 -0400

    Use JSX in example index file

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,13 +1,14 @@
 import Fastify from "fastify";
-import { App } from "./App.js";
+import React from "react";
 import { render, ImageFormat } from "react-trmnl";
+import { App } from "./App.js";
 
 const host: string = process.env.HOST ?? "localhost";
 const port: number = Number.isInteger(process.env.PORT) ? Number(process.env.PORT) : 3000;
 const server = Fastify();
 
 server.get("/display", async (_, rep) => {
-  const png: Buffer = await render(App(), ImageFormat.PNG);
+  const png: Buffer = await render(<App />, ImageFormat.PNG);
   rep.headers({ "Cache-Control": "no-cache", "Content-Type": "image/png" }).send(png);
 });
 
